604f28bf3205da91caafa8526f6a1b032c6014ed
Jehyun Jung <toojey7@gmail.com>
2022-05-27 11:38:19 +0900

    feat: add Websocket Connection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,21 @@
-alert("hi");
+//socket => represents a connection between front and back
+const socket=new WebSocket(`ws://${window.location.host}`);
 
+// Recieve message => (Events)
+
+//Connected to Server
+socket.addEventListener("open",()=>{
+    console.log("Connected to Server");
+});
+//Recieving message
+socket.addEventListener("message",(message)=>{
+    console.log("Just Got This: ",message.data,"from the server");
+});
+//Disconnected to Server
+socket.addEventListener("close",()=>{
+    console.log("Connection Closed");
+});
+
+setTimeout(()=>{
+    socket.send("hello from the browser")
+},10000);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,37 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 const app=express();
 
 app.set("view engine","pug");
 app.set("views",__dirname +"/views");
 app.use("/public",express.static(__dirname+ "/public"));
 app.get("/",(req,res) => res.render("home"));
+app.get("/*",(req,res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000,handleListen);  
+//app.listen(3000,handleListen);
+
+//http server => to get access to server
+const server=http.createServer(app);
+//websocket server
+const wss=new WebSocket.Server({server});
+
+//socket represents a connected web browser
+/*
+function handleConnection(socket){
+    console.log(socket)
+}
+*/
+wss.on("connection",(socket)=>{
+    console.log("Connected to Browser");
+    socket.on("close",()=>console.log("Disconnected from the Client"));
+    socket.on("message",(message)=>{
+        console.log(message.toString('utf-8'));
+    });
+    socket.send("hello!!!");
+});
+
+//using http server and websocket together
+server.listen(3000,handleListen);
+

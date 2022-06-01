aaad59855b24d63d763bbbe70852e1d2ce587d7a
Jehyun Jung <toojey7@gmail.com>
2022-06-01 17:55:01 +0900

    feat: Add Web RTC Connection Process

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,10 +4,20 @@ const myFace=document.getElementById("myFace");
 const muteButton=document.getElementById("mute");
 const cameraButton=document.getElementById("camera");
 const camerasSelect=document.getElementById("cameras");
+
+
+const call=document.getElementById("call");
+
+call.hidden=true;
+
 let myStream;
+
 
 let muted=false
 let cameraOff=false;
+let roomName;
+
+let myPeerConnection;
 
 async function getCameras(){
     try{
@@ -52,8 +62,6 @@ async function getMedia(deviceId){
 
 }
 
-getMedia();
-
 function handleMuteClick(){
     myStream.getAudioTracks().forEach(track=>track.enabled= !track.enabled)
     if(!muted){
@@ -77,9 +85,85 @@ function handleCameraClick(){
         cameraOff=false;
     }
 }
-function handleCameraSelectChange(){
+async function handleCameraSelectChange(){
     await getMedia(camerasSelect.value);
 }
 muteButton.addEventListener("click",handleMuteClick);
 cameraButton.addEventListener("click",handleCameraClick);
 camerasSelect.addEventListener("input",handleCameraSelectChange);
+
+
+// Welcome Foprm
+const welcome=document.getElementById("welcome");
+const welcomeForm=welcome.querySelector("form");
+async function startMedia(){
+    welcome.hidden=true;
+    call.hidden=false;
+    await getMedia();
+    makeConnection();
+}
+
+async function handleWelcomeSubmit(event){
+    event.preventDefault();
+    const input=welcomeForm.querySelector("input");
+    console.log(input.value);
+    await startMedia();
+    socket.emit("join_room",input.value);
+    roomName=input.value;
+    input.value="";
+}
+
+welcomeForm.addEventListener("submit",handleWelcomeSubmit)
+
+//Socket Code Signaling Process
+//PeerA
+socket.on("welcome",async ()=> {
+    console.log("some body joined");
+    const offer=await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer)    
+    socket.emit("offer",offer,roomName);
+})
+
+socket.on("answer",answer=>{
+    myPeerConnection.setRemoteDescription(answer);
+})
+
+//Peer B
+socket.on("offer",async (offer)=>{
+    myPeerConnection.setRemoteDescription(offer);
+    const answer=await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer",answer,roomName);
+})
+
+//Ice Candidate Handling
+socket.on("ice",(ice)=>{
+    myPeerConnection.addIceCandidate(ice);
+})
+
+//RTC Code
+/*
+WebRTC Connection을 만드는 과정이 socket.io/websockets보다 오래 걸리므로
+먼저 실행 시켜준다.
+*/
+function makeConnection(){
+    myPeerConnection=new RTCPeerConnection();3
+    myPeerConnection.addEventListener("icecandidate",handleIce)
+    myPeerConnection.addEventListener("addstreamevent",handleAddStream);
+    //add Tracks 대신에 사용하는 함수
+    myStream.getTracks().forEach(
+        track=> myPeerConnection.addTrack(track,myStream)
+        );
+
+}
+//Ice Candidates을 계속 전송하게 된다.
+function handleIce(data){
+    socket.emit("ice",data.candidate,roomName);
+}
+
+//받아온 Stream을 등록하면 상대방의 stream이 등록된다.
+function handleAddStream(data){
+    const peerFace=document.getElementById("peerFace");
+    peerFace.srcObject=data.stream;
+}
+
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import { errorMonitor } from "events";
 import express from "express";
 import http from "http";
 import SocketIO from "socket.io"
@@ -17,6 +18,26 @@ const server=http.createServer(app);
 
 //socket.io server
 const io=SocketIO(server);
+io.on("connection", (socket) => {
+    //새로운 방에 들어가고자 하면 socket를 방에 등록시켜준다.
+    socket.on("join_room",(roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    })
+    //offer
+    socket.on("offer",(offer,roomName) =>{
+        socket.to(roomName).emit("offer",offer);
+    });
+    //answer
+    socket.on("answer",(answer,roomName)=>{
+        socket.to(roomName).emit("answer",answer);
+    })
+    //ice candidate
+    socket.on("ice",(ice,roomName)=>{
+        socket.to(roomName).emit("ice",ice);
+    })
+});
+
 
 server.listen(3000,handleListen);
 
